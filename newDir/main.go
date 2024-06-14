03710d45c3869a479d122a5d7b911c8521a02145
zhaoxin-BF <1835783944@qq.com>
2024-06-14 11:44:20 +0800

    nothing

--- newDir/main.go
+++ newDir/main.go
@@ -9,12 +9,27 @@ import (
 func main() {
 	rootPath := "/Users/zhaoxin/Workspace/github.com/zhaoxin-BF/docker-test/newDir"
 	workerPath := path.Join(rootPath, "worker-3")
+	workerVolumePath := path.Join(workerPath, "empty", "worker-3-volume")
 
-	//err := os.MkdirAll(path.Join(rootPath, "worker-3", "empty"), 0777)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
+	err := os.MkdirAll(workerVolumePath, 0777)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	fileInstance, err := os.Create(path.Join(workerVolumePath, "username"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = fileInstance.Write([]byte("hello world"))
+	fileInstance.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(workerVolumePath)
 	//str, err := os.MkdirTemp(workerPath, "myapp-")
 	//if err != nil {
 	//	fmt.Println(err)
@@ -22,8 +37,8 @@ func main() {
 	//fmt.Println(str)
 
 	//time.Sleep(10 * time.Second)
-	err := os.RemoveAll(workerPath)
-	if err != nil {
-		fmt.Println(err)
-	}
+	//err := os.RemoveAll(workerPath)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 }
